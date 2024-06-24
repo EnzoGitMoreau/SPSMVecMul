d47b85e64bb5915e078cf93ead59cb47fa034baf
EnzoGitMoreau <135992892+EnzoGitMoreau@users.noreply.github.com>
2024-06-24 23:47:52 +0100

    dqs

--- testLauncher.sh
+++ testLauncher.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 # Loop through numbers 1 to 8
-for i in {1..12}
+for i in {1..20}
 do
   # Run the program with the current number as an argument
-  ./tests $i $(((12000 / $i)*$i)) 100 0.1
+  ./tests $i 12000 100 0.5
 
 done
